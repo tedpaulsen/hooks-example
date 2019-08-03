4b3b776f5df80aac71f29d7308716534fa92d408
Ted Paulsen <ted.m.paulsen@gmail.com>
2019-08-03 10:53:20 -0500

    update list item rendering

--- src/BreweryList.tsx
+++ src/BreweryList.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
+import { List, Avatar } from 'antd';
+
+import { OpenBreweryData } from './constants';
 
 const url = (region: string) =>
   `https://api.openbrewerydb.org/breweries?by_state=${region
@@ -28,10 +31,25 @@ export default function BreweryList(props: Props) {
   }, [props.region]);
 
   return (
-    <div>
-      {breweries.map((b: any) => (
-        <div key={b.id}>{b.name}</div>
-      ))}
-    </div>
+    <List
+      dataSource={breweries}
+      renderItem={(item: OpenBreweryData) => (
+        <List.Item key={item.id}>
+          <List.Item.Meta
+            avatar={
+              <Avatar
+                size={16}
+                style={{ marginTop: 8 }}
+                src={`https://www.google.com/s2/favicons?domain=${
+                  item.website_url
+                }`}
+              />
+            }
+            title={item.name}
+            description={item.city}
+          />
+        </List.Item>
+      )}
+    />
   );
 }
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,3 +1,5 @@
+import { AxiosResponse } from 'axios';
+
 export const allRegions = [
   'Alabama',
   'Alaska',
@@ -50,3 +52,24 @@ export const allRegions = [
   'Wisconsin',
   'Wyoming'
 ];
+
+export interface OpenBreweryData {
+  id: number;
+  name: string;
+  brewery_type: string;
+  street: string;
+  city: string;
+  state: string;
+  postal_code: string;
+  country: string;
+  longitude: string;
+  latitude: string;
+  phone: string;
+  website_url: string;
+  updated_at: string;
+  tag_list: string[];
+}
+
+export interface OpenBreweryResponse extends AxiosResponse {
+  data: OpenBreweryData[];
+}
